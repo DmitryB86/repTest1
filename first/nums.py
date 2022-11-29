3666fb3dd49c69fa79a82edc78ab693b8f2853f8
Dmitry Bogomolov <111758973+DmitryB86@users.noreply.github.com>
2022-11-29 19:12:28 +0300

    add function multiply create a new one modul

--- first/nums.py
+++ first/nums.py
@@ -1,11 +1,12 @@
 def plus(a,b):
     return a+b
 
-text = 'new function'
-
 def minus(a,b):
     return a-b
+def mul(a,b):
+    return a*b
 
 if __name__ == '__main__':
     print(plus(4,9))
     print(minus(34,11))
+    print(mul(21,16))
